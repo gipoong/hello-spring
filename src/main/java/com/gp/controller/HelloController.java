aeec94c7b35ed5bbd201d19aec2141e0e03a8fff
Gipoong <gipoong@naver.com>
2022-07-27 20:36:58 +0900

    Controller error 잡아야 함.

--- src/main/java/com/gp/controller/HelloController.java
+++ src/main/java/com/gp/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.gp.controller;
 
+import com.gp.domain.SampleDTO;
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String home(){
+    public String home(SampleDTO dto){
+
+      log.info("" + dto);
         return "index";
     }
 }
---
+++ src/main/java/com/gp/domain/SampleDTO.java
@@ -0,0 +1,10 @@
+package com.gp.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+
+    private String name;
+    private int age;
+}
